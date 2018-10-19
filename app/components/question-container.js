8ef7a8a6ffa67a46a3b45d53ae05dfda9b16db66
Noelle Daley <adriannenoelle@gmail.com>
2018-10-19 12:49:05 -0700

    start over on results page

--- app/components/question-container.js
+++ app/components/question-container.js
@@ -1,14 +1,11 @@
 import Component from "@ember/component";
-import { computed } from "@ember/object";
-import questions from "../helpers/questions";
 
 export default Component.extend({
-  score: 0,
-  currentIndex: 0,
-  isLastQuestion: computed("currentIndex", function() {
-    return this.currentIndex >= questions().length - 1;
-  }),
-  isDone: false,
+  score: null,
+  currentIndex: null,
+  isLastQuestion: null,
+  isDone: null,
+  startOver() {},
   actions: {
     handleForward: function() {
       if (this.isLastQuestion) {
