e4d297d771c9a7e588baf0bf511d841594e45f19
Noelle Daley <adriannenoelle@gmail.com>
2018-10-24 14:53:36 -0700

    show number of steps forward and backwards

--- app/components/results-page.js
+++ app/components/results-page.js
@@ -2,12 +2,10 @@ import Component from "@ember/component";
 import { computed } from "@ember/object";
 
 export default Component.extend({
-  // stepsForward: computed("answers", function() {
-  //   debugger
-  //   return this.answers.filter(a => a === 1).length;
-  // }),
-  // stepsBack: computed("answers", function() {
-  //   return this.answers.filter(a => a === -1).length;
-  // }),
-  answers: computed.alias("model.answers")
+  stepsForward: computed("answers", function() {
+    return this.answers.filter(a => a === 1).length;
+  }),
+  stepsBack: computed("answers", function() {
+    return this.answers.filter(a => a === -1).length;
+  })
 });
