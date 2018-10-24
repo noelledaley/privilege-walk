756f97872371ec54088c900d257e4de598c1dca1
Noelle Daley <adriannenoelle@gmail.com>
2018-10-24 13:42:44 -0700

    wip - show all answers on results page

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,10 +3,14 @@ import Controller from "@ember/controller";
 export default Controller.extend({
   sortProperties: "timestamp",
   sortAscending: false,
+  saveSuccess() {
+    this.send('doRefresh');
+  },
   actions: {
     saveAnswers: function(answers) {
       const newAnswers = this.store.createRecord("user-answers", answers);
-      newAnswers.save();
+      window.localStorage.setItem("privilegeWalkUserId", JSON.stringify(newAnswers.id));
+      newAnswers.save().then(() => this.saveSuccess());
     }
   }
 });
