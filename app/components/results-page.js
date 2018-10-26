4438d4123b3f5fcdfb4840b6f489202a7602d23e
Noelle Daley <adriannenoelle@gmail.com>
2018-10-26 11:28:08 -0700

    show average score; make score bars green or red

--- app/components/results-page.js
+++ app/components/results-page.js
@@ -12,15 +12,9 @@ export default Component.extend({
   stepsBack: computed("parentModel.answers", function() {
     return this.answers.filter(a => a === -1).length;
   }),
-  percentage: computed("results", function() {
+  averageScore: computed("results", function() {
     const allScores = this.model.map(i => i.score);
     const sumScores = allScores.reduce((prev, current) => (current += prev));
-    const avg = sumScores / allScores.length;
-    const percentage = ((this.score / avg) * 100).toFixed(0);
-    if (percentage >= 0) {
-      return `${percentage}% more`;
-    } else {
-      return `${Math.abs(percentage)}% less`;
-    }
+    return sumScores / allScores.length;
   })
 });
--- app/components/score-bar.js
+++ app/components/score-bar.js
@@ -1,3 +1,6 @@
 import Component from "@ember/component";
+import { computed } from "@ember/object";
 
-export default Component.extend({});
+export default Component.extend({
+  isPositive: computed.gte("score", 0)
+});
